1d0e5cba1d3fc3e247a95c4a91502e13990e90c1
Dazzle Basa <dazdazah@users.noreply.github.com>
2018-04-30 22:07:01 -0400

    Trip_Trio_project

--- homework/citi_pix/js/app.js
+++ homework/citi_pix/js/app.js
@@ -2,24 +2,9 @@
 // When I change my selection
 // I expect the background image to change into the image of my selection
 
-// $("#city-type").change(changeCity);
-
-// function changeCity() {
-//   var userSelection = $("#city-type").val();
-
-//   $("   ").addClass(userSelection);
-// }
-
 $("#city-type").change(changeCity);
 
 function changeCity() {
   var userSelection = $("#city-type").val();
-  if (userSelection === nyc){
-  	 $("body").addClass("nyc")
-  } else if (userSelection === sf){
-  	 $("body").addClass("sf")
-  } else if(userSelection === la){
-  	 $("body").addClass("la")
-  }
-
+  $('body').addClass(userSelection);
 }
